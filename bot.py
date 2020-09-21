e8262fe42685e97c14f3a45ec22694328644ff93
DrakePork <drakepork@hotmail.com>
2020-09-21 00:17:42 +0000

    wham

--- bot.py
+++ bot.py
@@ -25,7 +25,19 @@ async def on_message(message):
             channel = client.get_channel(int(748501274944602214))
             await channel.send(file=discord.File('/home/ubuntu/python/catbot/pics/cat1.jpg'))
 
-    if "katt" or "cat" or "pussy" in message.content.lower():
+    if "cat" in message.content.lower():
+        await catLoad(message)
+
+    if "katt" in message.content.lower():
+        await catLoad(message)
+
+    if "pussy" in message.content.lower():
+        await catLoad(message)
+
+    if "kat" in message.content.lower():
+        await catLoad(message)
+
+    if "neko" in message.content.lower():
         await catLoad(message)
 
 with open ("/home/ubuntu/python/catbot/token.txt", "r") as myfile:

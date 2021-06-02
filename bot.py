3dc2bcc1086f83dc2a654ad05c3a95bf17d9104d
DrakePork <drakepork@hotmail.com>
2021-06-02 19:54:10 +0200

    Added some more panda pics Added some slash commands

--- bot.py
+++ bot.py
@@ -1,12 +1,36 @@
 import discord
 import random
+from discord_slash import SlashCommand
 
 client = discord.Client()
+
+slash = SlashCommand(client, sync_commands=True)
 
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
+
+@slash.slash(name="cat", description="Shows cat")
+async def cat_pic(ctx):
+    await picLoad("cat", ctx)
+
+
+@slash.slash(name="redpanda", description="Shows red panda")
+async def cat_pic(ctx):
+    await picLoad("redpanda", ctx)
+
+
+async def picLoad(aniType, ctx):
+    from os import listdir
+    from os.path import isfile, join
+    if aniType is "redpanda":
+        folder = "/home/ubuntu/python/catbot/redpandapics"
+    elif aniType is "cat":
+        folder = "/home/ubuntu/python/catbot/catpics"
+    animal = [f for f in listdir(folder) if isfile(join(folder, f))]
+    await ctx.channel.send(file=discord.File(folder + '/' + random.choice(animal)))
 
 
 @client.event
@@ -21,7 +45,7 @@ async def on_message(message):
             from os import listdir
             from os.path import isfile, join
             if type is "panda":
-                folder = "/home/ubuntu/python/catbot/pandapics"
+                folder = "/home/ubuntu/python/catbot/redpandapics"
             elif type is "cat":
                 folder = "/home/ubuntu/python/catbot/catpics"
             animal = [f for f in listdir(folder) if isfile(join(folder, f))]
